3fb4a8246657e505711036c21d2fa33a6ea8820c
GitHub Action <action@github.com>
2024-09-20 00:36:16 +0000

    Template cleanup

--- src/main/kotlin/com/github/hackerorange/glodonframeworkupgradeplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/hackerorange/glodonframeworkupgradeplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.hackerorange.glodonframeworkupgradeplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
