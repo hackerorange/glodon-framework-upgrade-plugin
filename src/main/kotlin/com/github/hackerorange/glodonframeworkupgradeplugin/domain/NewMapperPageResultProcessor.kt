b47182f72256f299aa843c287e447d20b2e968cd
zhongct <zhongct@glodon.com>
2024-09-20 20:44:47 +0800

    submit

--- src/main/kotlin/com/github/hackerorange/glodonframeworkupgradeplugin/domain/NewMapperPageResultProcessor.kt
+++ src/main/kotlin/com/github/hackerorange/glodonframeworkupgradeplugin/domain/NewMapperPageResultProcessor.kt
@@ -7,45 +7,84 @@ import com.intellij.psi.search.GlobalSearchScope
 
 class NewMapperPageResultProcessor : PsiFileProcessor {
 
-    private var newBaseMapper: PsiClass? = null
-
+    private var mybatisPlusPageClass: PsiClass? = null
+    private var listClass: PsiClass? = null
 
     override fun processPsiFile(project: Project, psiFile: PsiFile) {
-        if (newBaseMapper == null) {
+        if (mybatisPlusPageClass == null || listClass == null) {
             return
         }
 
         psiFile.accept(object : JavaRecursiveElementVisitor() {
 
-            override fun visitMethodCallExpression(psiMethodCallExpression: PsiMethodCallExpression) {
+            override fun visitAssignmentExpression(expression: PsiAssignmentExpression) {
+                val lExpression = expression.lExpression
+                val rExpression = expression.rExpression ?: return
+                extracted(lExpression.type ?: return, rExpression.type ?: return, project, rExpression)
+                super.visitAssignmentExpression(expression)
+            }
 
-                val resolveMethod = psiMethodCallExpression.resolveMethod()
+            override fun visitDeclarationStatement(statestatementment: PsiDeclarationStatement) {
 
-                if (resolveMethod != null) {
-                    if (newBaseMapper == resolveMethod.containingClass) {
-                        if (psiMethodCallExpression.methodExpression.referenceName == "selectPage") {
-                            WriteCommandAction.runWriteCommandAction(project) {
-                                val methodNewIdentity =
-                                    JavaPsiFacade.getInstance(project).elementFactory.createExpressionFromText(
-                                        "${psiMethodCallExpression.text}.getRecords()",
-                                        psiMethodCallExpression
-                                    )
-                                psiMethodCallExpression.replace(methodNewIdentity);
-                            }
+                for (declaredElement in statestatementment.declaredElements) {
+
+                    if (declaredElement is PsiLocalVariable) {
+                        val variableType = declaredElement.type
+                        val psiExpression = declaredElement.initializer ?: continue
+
+                        val initialType = psiExpression.type ?: return
+                        if (initialType == variableType) {
+                            continue
                         }
+                        extracted(variableType, initialType, project, declaredElement.initializer!!)
                     }
                 }
-
-                super.visitMethodCallExpression(psiMethodCallExpression)
+                super.visitDeclarationStatement(statestatementment)
             }
         })
 
     }
 
+    private fun extracted(
+        leftType: PsiType,
+        rightType: PsiType,
+        project: Project,
+        rightExpression: PsiExpression
+    ) {
+        if (leftType is PsiClassType && rightType is PsiClassType) {
+
+
+            val variableTypeClass = leftType.resolve() ?: return
+            val initialTypeClass = rightType.resolve() ?: return
+
+
+            val isVariableList =
+                variableTypeClass == listClass || variableTypeClass.isInheritor(listClass!!, true)
+            val isInitialPage =
+                initialTypeClass == mybatisPlusPageClass || initialTypeClass.isInheritor(
+                    mybatisPlusPageClass!!,
+                    true
+                )
+            if (isVariableList && isInitialPage) {
+                WriteCommandAction.runWriteCommandAction(project) {
+                    val methodNewIdentity =
+                        JavaPsiFacade.getInstance(project).elementFactory.createExpressionFromText(
+                            "${rightExpression.text}.getRecords()", null
+                        )
+                    rightExpression.replace(methodNewIdentity);
+                }
+            }
+        }
+    }
+
 
     override fun init(project: Project) {
-        newBaseMapper = JavaPsiFacade.getInstance(project).findClass(
-            "com.baomidou.mybatisplus.core.mapper.BaseMapper",
+        mybatisPlusPageClass = JavaPsiFacade.getInstance(project).findClass(
+            "com.baomidou.mybatisplus.core.metadata.IPage",
+            GlobalSearchScope.allScope(project)
+        )
+        listClass = JavaPsiFacade.getInstance(project).findClass(
+            "java.util.List",
             GlobalSearchScope.allScope(project)
         )
     }
