be6fb948fded6810ed83ef6fde08f4746747a870
zhongct <zhongct@glodon.com>
2024-09-22 13:20:41 +0800

    submit

--- src/main/kotlin/com/github/hackerorange/glodonframeworkupgradeplugin/inspections/MoveAndStatementToLambdaExpression.kt
+++ src/main/kotlin/com/github/hackerorange/glodonframeworkupgradeplugin/inspections/MoveAndStatementToLambdaExpression.kt
@@ -3,6 +3,7 @@ package com.github.hackerorange.glodonframeworkupgradeplugin.inspections
 import com.intellij.codeInspection.AbstractBaseJavaLocalInspectionTool
 import com.intellij.codeInspection.LocalQuickFixAndIntentionActionOnPsiElement
 import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
 import com.intellij.psi.*
@@ -12,8 +13,6 @@ import com.intellij.refactoring.util.CommonRefactoringUtil
 class MoveAndStatementToLambdaExpression : AbstractBaseJavaLocalInspectionTool() {
 
     override fun buildVisitor(problemsHolder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
-
-        println("112321313")
 
         return object : JavaRecursiveElementVisitor() {
             override fun visitMethodCallExpression(methodCallExpression: PsiMethodCallExpression) {
@@ -61,8 +60,8 @@ class MoveAndStatementToLambdaExpression : AbstractBaseJavaLocalInspectionTool()
                 return
             }
 
-            val myHandler = MyHandler()
-            myHandler.invoke(project, startElement, editor)
+            val myHandler = MyHandler(startElement as PsiMethodCallExpression)
+            myHandler.invoke(project, editor, startElement.containingFile, null)
         }
 
         override fun startInWriteAction(): Boolean {
@@ -79,13 +78,56 @@ class MoveAndStatementToLambdaExpression : AbstractBaseJavaLocalInspectionTool()
         }
     }
 
-    class MyHandler : IntroduceVariableBase() {
+    class MyHandler(private val psiMethodCallExpression: PsiMethodCallExpression) : IntroduceVariableBase() {
         override fun showErrorMessage(project: Project?, editor: Editor?, message: String?) {
             println("发生异常")
         }
 
-        override fun invokeImpl(project: Project?, expr: PsiExpression?, editor: Editor?): Boolean {
-            println(expr?.text)
+        override fun invokeImpl(project: Project, psiExpression: PsiExpression, editor: Editor?): Boolean {
+            val selectText = psiExpression.text ?: return true
+
+            val subText = psiMethodCallExpression.text
+
+            if (psiMethodCallExpression == psiExpression) {
+                return true
+            }
+
+            if (selectText.contains(subText)) {
+                val aaa = selectText.replace(subText, "")
+                println(aaa)
+
+                val replacedExpression = psiMethodCallExpression.copy() as PsiMethodCallExpression
+
+                println(replacedExpression.methodExpression.referenceNameElement)
+
+                val createIdentifier = JavaPsiFacade.getInstance(project).elementFactory.createIdentifier("and")
+                replacedExpression.methodExpression.referenceNameElement?.replace(createIdentifier)
+
+                replacedExpression.argumentList.add(
+                    JavaPsiFacade.getInstance(project).elementFactory.createExpressionFromText(
+                        "true",
+                        null
+                    )
+                )
+
+                replacedExpression.argumentList.add(
+                    JavaPsiFacade.getInstance(project).elementFactory.createExpressionFromText(
+                        "qw->{qw${aaa};}",
+                        null
+                    )
+                )
+
+                WriteCommandAction.runWriteCommandAction(project) {
+                    psiExpression.replace(replacedExpression)
+                }
+
+
+            }
+
+
+
+
+
             return true
         }
 
